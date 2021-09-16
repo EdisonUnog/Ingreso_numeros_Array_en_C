0833f140094501aa9d21ca5b4b9e12a01631fbcf
Edison_Uñog <edisoncmac@gmail.com>
2021-09-16 10:56:51 -0300

    Ingreso_Numeros_Array

--- src/funciones.c
+++ src/funciones.c
@@ -34,20 +34,24 @@ int utn_getNumero(int* pResultado, char*mensaje, char*mensajeError, int reintent
 int sumaNumeros(int arrayNumerosss[],int len){
 	int i;
 	int acumSuma=0;
-		for(i=0;i<len;i++){
+	if(arrayNumerosss!=NULL && len>=0){
+	    for(i=0;i<len;i++){
 			acumSuma+=arrayNumerosss[i];
 		}
-		return acumSuma;
 	}
+	return acumSuma;
+}
 //-----------PROMEDIO_DE_NUMEROS-------------------------------------------------
 float promedioNumeros(int arrayNumeross[],int len){
 	int i;
 	int acumSuma=0;
 	float prom;
-	for(i=0;i<len;i++){
-		acumSuma+=arrayNumeross[i];
+	if(arrayNumeross!=NULL && len>=0){
+	 for(i=0;i<len;i++){
+			acumSuma+=arrayNumeross[i];
+		}
+	 prom=(float)acumSuma/len;
 	}
-	prom=(float)acumSuma/len;
 	return prom;
 }
 //-----------IMPRIMIR_RESULTADO--------------------------------------------------
